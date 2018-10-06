86c61c134eb8dce1e1ebc5ddf212121367b88203
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-10-05 22:22:27 -0400

    Styling changes, adding navbar (with no links, though)

--- src/HomePage.js
+++ src/HomePage.js
@@ -13,10 +13,23 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div class="container">
+        <p className="hed">
+          <a href="/work">
+            <span>Work</span>
+          </a>{' '}
+          /{' '}
+          <a href="/about">
+            <span>About</span>
+          </a>{' '}
+          /{' '}
+          <a href="/contact">
+            <span>Contact</span>
+          </a>
+        </p>
         <h1 class="intro-hed">
-          I’m <a href="http://rambhajan.me/#">Aaron Rambhajan</a>. I develop
-          interactive experiments, help bring bikes to the world, and create
-          music.
+          I’m <a href="http://rambhajan.me/#">Aaron Rambhajan</a>. I bring my
+          world to life through software engineering, behavioural research, and
+          music production.
         </h1>
 
         {/* <Options /> */}
