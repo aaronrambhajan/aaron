0fa7a1dfdbb36af8d635366e00d69ce6c0ee073a
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-12-11 14:40:10 -0500

    Abbreviating contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -8,25 +8,8 @@ import Soundcloud from '../images/Soundcloud';
 export default class ContactPage extends React.Component {
   render = () => {
     return (
-      <div>
-        <h1 className="intro-hed">
-          Catch me chirping with the birds on{' '}
-          <a href="https://twitter.com/AARONRAMBHAJAN">
-            <Twitter size={24} />
-          </a>
-          , snappin' around on{' '}
-          <a href="https://instagram.com/aaronrambhajan/">
-            <Instagram size={24} />
-          </a>
-          , making noise on{' '}
-          <a href="https://soundcloud.com/rambhajan">
-            <Soundcloud size={24} />
-          </a>
-          , or just send me an{' '}
-          <a href="mailto:aaron.rambhajan@mail.utoronto.ca">email</a>. If those
-          don't suit your fancy, you can find my LinkedIn and GitHub below. I'm
-          always looking for new things to learn.
-        </h1>
+      <div class="contact">
+        <p />
       </div>
     );
   };
