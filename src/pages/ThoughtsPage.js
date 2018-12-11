fa81302138fd051ff1766b0227a2e8c118d0bc8b
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-12-11 14:42:52 -0500

    A quick little addition for some pocket, hidden writing

---
+++ src/pages/ThoughtsPage.js
@@ -0,0 +1,21 @@
+// @flow
+
+  import React from 'react';
+
+export default class ThoughtsPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p className="thoughts">October 6th, 2018</p>
+        <p className="writing">
+          There are so many things that I care about and love, and I'm so
+          grateful for it all. In the coming months, I really want to focus on a
+          few things. Linear algebra, statistics, and harmony are what I'm most
+          curious about right now. I'm normally quite overwhelmed with life so I
+          don't really know where I'll make time for it, but I know that if I'm
+          really interested I will move towards it.
+        </p>
+      </div>
+    );
+  }
+}
