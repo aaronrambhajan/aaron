2aaf9b47de9d95e538d3ab8378c586d2c8820714
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-12-11 14:43:31 -0500

    Adding a link to get to the ADHD Page

--- src/pages/WorkPage.js
+++ src/pages/WorkPage.js
@@ -3,13 +3,18 @@ import React from 'react';
 export default class WorkPage extends React.Component {
   render = () => {
     return (
-      <h1 class="intro-hed">
-        You can download my resume{' '}
-        <a href="https://www.dropbox.com/s/600rbuu5ydpo8lk/Aaron-Rambhajan-Resume.pdf?dl=0">
-          here
-        </a>
-        .
-      </h1>
+      <div>
+        <h1 class="intro-hed">
+          You can download my resume{' '}
+          <a href="https://www.dropbox.com/s/600rbuu5ydpo8lk/Aaron-Rambhajan-Resume.pdf?dl=0">
+            here
+          </a>
+          .
+        </h1>
+        <p>
+          A project about <a href="/projects/ADHD">ADHD</a>.
+        </p>
+      </div>
     );
   };
 }
