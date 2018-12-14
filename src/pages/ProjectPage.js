9f97caac23148e7fc355b311df00362350b5dc28
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-12-14 00:25:00 -0500

    Lots of image and project display changes

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -2,23 +2,53 @@
 
 import React from 'react';
 import { Button, Collapse } from 'reactstrap';
+
+// Components
 import Persona from '../components/Persona';
 import ProjectTitle from '../components/ProjectTitle';
 import { ButtonGroupings } from '../components/ProjectCard';
 import ImageRow from '../components/ImageRow';
-import Asana from '../images/ADHD/Asana';
-import Trello from '../images/ADHD/Trello';
-import Todoist from '../images/ADHD/Todoist';
+import ImageWithSideCaption from '../components/ImageWithSideCaption';
+import ImageWide from '../components/ImageWide';
 
-export default class HomePage extends React.Component {
+/* IMAGES */
+// Apps
+import appAsana from '../images/ADHD/Asana';
+import appTrello from '../images/ADHD/Trello';
+import appTodoist from '../images/ADHD/Todoist';
+
+// Story
+import storyMessyRoomBlank from '../images/ADHD/messy-room-blank.png';
+import storyMessyRoom from '../images/ADHD/natalia-room-adhd.gif';
+import storyTextAnxious from '../images/ADHD/text-anxious.gif';
+import storyTextSafe from '../images/ADHD/text-safe.gif';
+
+// Needs
+import needsAccountability from '../images/ADHD/needs-accountability.svg';
+import needsAwareness from '../images/ADHD/needs-awareness.svg';
+import needsFocus from '../images/ADHD/needs-focus.svg';
+import needsGuilt from '../images/ADHD/needs-guilt.svg';
+
+// Process
+import procIdeation from '../images/ADHD/ideation.png';
+import procLowFidelity from '../images/ADHD/low-fidelity.png';
+import procMidFidelity from '../images/ADHD/medium-fidelity.png';
+import procPhone1 from '../images/ADHD/full-1.png';
+import procPhone2 from '../images/ADHD/full-2.png';
+import procPhone3 from '../images/ADHD/full-3.png';
+import procPhoneFull from '../images/ADHD/phone.png';
+
+export default class ProjectPage extends React.Component {
   state: {
     problem: boolean,
     story: boolean,
+    credits: boolean,
   };
 
   state = {
-    problem: false,
+    problem: true,
     story: false,
+    credits: false,
   };
 
   toggleProblem = () => {
@@ -29,9 +59,14 @@ export default class HomePage extends React.Component {
     this.setState({ story: !this.state.story });
   };
 
+  toggleCredits = () => {
+    this.setState({ credits: !this.state.credits });
+  };
+
   render = () => {
     return (
       <div class="projects">
+        {/* SUMMARY */}
         <div class="projects-header">
           <h1
             class="intro-hed"
@@ -59,18 +94,18 @@ export default class HomePage extends React.Component {
 
         <hr />
 
+        {/* SECTION 1 */}
         <div class="project-article">
           <ProjectTitle
             toggle={this.toggleStory}
             title="Story"
             titleColor={this.state.story ? 'grey' : 'black'}
-            // backgroundColor={this.state.story ? 'lavender' : 'white'}
           />
 
           <Collapse isOpen={this.state.story}>
+            {/* @todo: Add tooltips to the symptoms. */}
             <Persona />
 
-            {/* @todo: Add tooltips to the symptoms. */}
             <p>
               Having ADHD, Natalia struggles with <strong>impulsivity</strong>,{' '}
               <strong>hyperactivity</strong>, and <strong>inattention</strong>.
@@ -81,6 +116,48 @@ export default class HomePage extends React.Component {
                 <strong>impossible</strong>.
               </code>
             </p>
+
+            <p>
+              Tonight, Natalia has plans with an old friend,{' '}
+              <strong>Rebeka</strong>. They went to elementary school together,
+              and they're seeing each other for the first time in a year. As you
+              can imagine, Natalia's super excited. She comes home from class in
+              the afternoon, and heads to her room to get ready. But when she
+              gets in... Her room is a mess.
+            </p>
+
+            <ImageWide
+              image={storyMessyRoomBlank}
+              alt="messy room"
+              caption="Natalia's Room"
+            />
+
+            <p>
+              Natalia feels like she <strong>needs</strong> to clean it. Well,
+              it's not so much that she needs to. She's getting anxious and
+              starting to freak out. Really, it's because she's{' '}
+              <strong>hyperfocusing</strong>.
+            </p>
+
+            <p>
+              I know what you're thinking.{' '}
+              <i>Why can't she just... clean it later?</i> Totally fair. After
+              all, that's what most of us would do. Here's the thing, though.
+              See, people with ADHD experience focus in <i>extremes</i>. Most of
+              us think of ADHD as young children, bouncing off the walls, being
+              easily distracted. But the opposite is also common—being so
+              focused into something that she forgets the rest of the world.
+            </p>
+            <p>
+              {' '}
+              What you need to understand, is that Natalia doesn't just see a
+              messy room. <strong>This</strong> is what she sees.
+            </p>
+            <ImageWide
+              image={storyMessyRoom}
+              alt="messy room animation"
+              caption="What she sees"
+            />
           </Collapse>
           <hr />
 
@@ -91,59 +168,96 @@ export default class HomePage extends React.Component {
           />
 
           <Collapse isOpen={this.state.problem}>
+            <p>
+              ADHD is complicated. When most people think of ADHD, they think of
+              children bouncing off the walls, being easily distractable, or a
+              friend who's just always fidgeting. What most people don't know is
+              that the reality is that people with ADHD experience focus in{' '}
+              <strong>extremes</strong>. See, dealing with ADHD is
+              complicated—even though we know their symptoms are impulsivity,
+              hyperactivity, and inattention, we don't understand what those{' '}
+              <i>mean</i>. How are they experienced? How are they <i>felt?</i>
+            </p>
+
+            <p>
+              Unsurprisingly, people who have ADHD struggle with productivity.
+              Like many of us, they're (clinical) procrastinators, tending to do
+              things only when they feel urgent—more often than not, that means
+              it's happening at the last minute. The thing is, it's not that
+              simple. They <i>do</i> get things done. It's just that it's
+              draining. It only happens at the last minute. It only happens when
+              they hyperfocus. <strong>They don't have control over it</strong>.
+            </p>
+
+            <h3 style={{ fontWeight: 700 }}>Research</h3>
+
+            <p>
+              So, how do people with ADHD deal with that right now? How do they
+              manage their lives, right now? We talked to them to find out. We
+              found out that they struggle with this thing and that thing, and
+              these symptoms affect them the most. Three apps that came up
+              during our primary research were:
+            </p>
+
             <ImageRow
-              img1={Trello}
+              img1={appTrello}
               img1txt="Trello"
-              img2={Asana}
+              img2={appAsana}
               img2txt="Asana"
-              img3={Todoist}
+              img3={appTodoist}
               img3txt="Todoist"
+              size={90}
             />
 
             <p>
-              Today's recipes Apps do not consider how to use present
-              ingredients in people’s kitchens best. In this case, people have
-              specific kinds of ingredients in their refrigerator, but they have
-              no ideas how to mix them up or cook in an apparent way.
-            </p>
-            <h2>Approaches</h2>
-            <p>
-              This App provides users with a mobile experience that offers
-              accessible recipes based on what ingredients they own to enhance
-              their lives in a healthy and environment-friendly way.
-            </p>
-            <h2>Objectives</h2>
-            <p>
-              Provide the users more personalized and healthy recipe
-              recommendations when they have no idea about how to mixing the
-              exist ingredients to enhance the users’ life quality and to reduce
-              food waste.
-            </p>
-            <p>
-              Incorporating the local food markets, the App could increase the
-              sales volume of local food according to the recipe
-              recommendations.
-            </p>
-            <p>
-              By recording people’s dinning habits, it could help people to form
-              a better lifestyle and help people to discover their lives via
-              foods.
+              They work fine for most people. The issue is that{' '}
+              <strong>these apps weren't built for ADHD</strong>. They're just
+              reminders. They don't help people with ADHD because they just
+              forget. Well, not quite. Let me explain why they don't work—
             </p>
 
-            <h2>Persona</h2>
-            <Persona />
-            <h2>User Needs</h2>
+            <ImageWithSideCaption
+              text="Notification systems built for her symptoms that keep her self-aware"
+              image={needsAccountability}
+              alt="needs-accountability"
+            />
+
+            <ImageWithSideCaption
+              text="Someone who can empathize with her and hold her accountable"
+              image={needsAwareness}
+              alt="needs-awareness"
+            />
+
             <p>
-              High-quality, fresh, healthy and time-based recommendations in the
-              front page.
+              The current apps fail on these specific dimensions. So... we
+              started thinking, how can we address that?
+            </p>
+
+            <h3 style={{ fontWeight: 700 }}>Big Ideas</h3>
+
+            <ImageWide image={procIdeation} alt="big ideas" />
+            <ImageWide image={storyTextAnxious} alt="storytime" />
+          </Collapse>
+
+          <hr />
+          <ProjectTitle
+            toggle={this.toggleCredits}
+            title="Credits"
+            titleColor={this.state.credits ? 'grey' : 'black'}
+          />
+
+          <Collapse isOpen={this.state.credits}>
+            <p>
+              Part of <strong>INF1602</strong>, Fundamentals of User Experience.
+              Taught by Professor Olivier St-Cyr as a part of the Master of
+              Information program at the University of Toronto.
             </p>
             <p>
-              Clear interface to show the recipe details. It should have good
-              readability.
-            </p>
-            <p>
-              Online shopping and ingredient delivery. It may include shopping
-              list for offline use.
+              <strong>Team</strong>:{' '}
+              <a href="https://alexandrascandolo.com" alt="Alexandra Scandolo">
+                Alexandra Scandolo
+              </a>
+              , Ali Jafar, Christina Vuong, Olivia Zhang{' '}
             </p>
           </Collapse>
         </div>
