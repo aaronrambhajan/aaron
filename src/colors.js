33423a103267f58041666a2f9a381441d690abd7
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2019-01-09 14:56:53 -0500

    Updating with accurate colours

--- src/colors.js
+++ src/colors.js
@@ -2,11 +2,11 @@
 
 export const colors = {
   STANDARD: 'rgba(0, 0, 0, 1.0)',
-  LAB_PRIMARY: 'rgba(0, 173, 239, 1.0)', // #00adef
-  LAB_SECONDARY: 'rgba(26, 37, 72, 1.0)', // #1a2548
-  YES_BUTTON: 'rgba(87, 167, 0, 1.0)', // #57A700
-  NO_BUTTON: 'rgba(255, 0, 0, 1.0)', // #FF0000
-  DISABLED_BUTTON: 'rgba(211, 211, 211, 1.0)', // #D3D3D3
+  SECONDARY: 'rgba(69, 69, 69, 1.0)', // #454545
+  BLUE_PRIMARY: 'rgba(0, 173, 239, 1.0)', // #00adef
+  BLUE_SECONDARY: 'rgba(26, 37, 72, 1.0)', // #1a2548
+  PINK_PRIMARY: 'rgba(232, 62, 140, 1.0)', // #E83E8C
+  PINK_SECONDARY: 'rgba(255, 240, 245, 1.0)', // #FFF0F5
 };
 
 export const changeOpacity = (color: String, newOpacity: String) => {
