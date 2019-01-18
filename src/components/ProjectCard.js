2c8cf6cce79636bb1d942c4184ca918625e53800
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2019-01-18 01:51:48 -0500

    can finally get rid of the case statement buttons b/c i got over myself

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,121 +1,105 @@
 // @flow
 
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Badge, Alert } from 'reactstrap';
 import ImageWide from '../components/ImageWide';
-import Chevron from '../images/Chevron.svg';
+import Chevron from '../images/Chevron';
+import { colors, changeOpacity } from '../colors';
 
 export default class ProjectCard extends React.Component {
   props: {
     href: String,
-    text: Object, // .title, .text
+    text: Object, // .title, .text, .info {date, context, status}
     image: Object, // .img, .alt, .caption
     labels: Array,
   };
 
   render = () => {
     return (
-      <div class="project-card">
-        <h1
-          class="intro-hed"
-          style={{ color: 'black', fontWeight: 600, marginBottom: 0 }}
-        >
-          {this.props.text.title}
-        </h1>
-
-        {this.props.text.description}
-
-        {!!this.props.image && (
-          <ImageWide
-            image={this.props.image.img}
-            alt={this.props.image.alt}
-            caption={{ text: this.props.image.caption }}
-            style={{ maxHeight: 250, width: 'auto' }}
-          />
-        )}
-
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-          }}
-        >
-          <div class="skill-group">
-            {this.props.labels.map((label) => {
-              return ButtonGroupings(label);
-            })}
+      <button class="project-card">
+        <a href={this.props.href}>
+          {/* title */}
+          <div>
+            <h1
+              class="intro-hed"
+              style={{ color: 'black', fontWeight: 600, marginBottom: 0 }}
+            >
+              {this.props.text.title}
+            </h1>
+            <p
+              style={{
+                color: changeOpacity(colors.SECONDARY, '0.5'),
+                fontSize: '0.6em',
+                margin: 0,
+                marginBottom: 10,
+                fontFamily: 'times',
+                fontWeight: 100,
+              }}
+            >
+              {this.props.text.info}
+            </p>
           </div>
+
+          {!!this.props.text.description && this.props.text.description}
+
+          {!!this.props.image && (
+            <ImageWide
+              image={this.props.image.img}
+              alt={this.props.image.alt}
+              caption={{ text: this.props.image.caption }}
+              style={{ maxHeight: 250, width: 'auto' }}
+            />
+          )}
+
           <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <div class="skill-group">
+              {this.props.labels.map((label) => {
+                return ButtonGroupings(label);
+              })}
+            </div>
+
+            {/* <div
             style={{
               display: 'flex',
               flexDirection: 'row',
               justifyContent: 'center',
               alignItems: 'center',
             }}
-          >
-            <a href={this.props.href} style={{ fontSize: '10' }}>
-              See more
-            </a>
-            <div style={{ marginRight: 10 }} />
-            <img
-              src={Chevron}
-              alt="Chevron"
-              style={{ height: 10, width: 10, fill: 'blue' }}
-            />
+          > */}
+            {/* <Chevron
+              size={20}
+              colour={colors.BLUE_SECONDARY}
+              style={{ marginRight: '5%' }}
+            /> */}
+            {/* </div> */}
           </div>
-        </div>
-      </div>
+        </a>
+      </button>
     );
   };
 }
 
 export function ButtonGroupings(label) {
-  // colors:
-  //  primary, secondary, success, info, danger, warning
-  switch (label) {
-    case 'User Research':
-      return (
-        <Button disabled color="danger" size="sm" style={{ marginRight: 10 }}>
-          User Research
-        </Button>
-      );
-    case 'Data Analysis':
-      return (
-        <Button disabled color="info" size="sm" style={{ marginRight: 10 }}>
-          Data Analysis
-        </Button>
-      );
-    case 'UX/UI Design':
-      return (
-        <Button disabled color="warning" size="sm" style={{ marginRight: 10 }}>
-          UX/UI Design
-        </Button>
-      );
-    case 'Experimental Design':
-      return (
-        <Button disabled color="primary" size="sm" style={{ marginRight: 10 }}>
-          Experimental Design
-        </Button>
-      );
-    case 'Cognitive Psychology':
-      return (
-        <Button disabled color="success" size="sm" style={{ marginRight: 10 }}>
-          Cognitive Psychology
-        </Button>
-      );
-    case 'Programming':
-      return (
-        <Button
-          disabled
-          color="secondary"
-          size="sm"
-          style={{ marginRight: 10 }}
-        >
-          Programming
-        </Button>
-      );
-    default:
-      return;
-  }
+  const buttonStyle = {
+    fontSize: '0.6em',
+    padding: 8,
+    fontWeight: 400,
+    marginRight: 10,
+    opacity: 0.6,
+    borderRadius: 15,
+    backgroundColor: 'transparent',
+  };
+
+  return (
+    <Button outline color="secondary" size="sm" style={buttonStyle}>
+      {label}
+    </Button>
+  );
 }
