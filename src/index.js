c19cef6270697adadc07178246d701655c847612
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2019-01-24 00:43:02 -0500

    redesigning navigation

--- src/index.js
+++ src/index.js
@@ -7,37 +7,33 @@ import HomePage from './pages/HomePage';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import AboutPage from './pages/AboutPage';
-import WorkPage from './pages/WorkPage';
 import MusicPage from './pages/MusicPage';
 import ContactPage from './pages/ContactPage';
 import ADHDProject from './projects/ADHD';
-import TrashProject from './projects/Trash';
 import HeartbeatProject from './projects/Heartbeats';
 import TrashFieldStudyProject from './projects/TrashFieldStudy';
 
+// [Nested Routing](https://stackoverflow.com/questions/41474134/nested-routes-with-react-router-v4)
+
 const App = () => (
   <Router>
-    <div class="container">
+    <div>
       <Navigation />
+      <div class="container">
+        <div>
+          <Route exact path="/" component={HomePage} />
 
-      <div>
-        <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route path="/music" component={MusicPage} />
+          <Route path="/contact" component={ContactPage} />
 
-        <Route exact path="/about" component={AboutPage} />
-        <Route path="/work" component={WorkPage} />
-        <Route path="/music" component={MusicPage} />
-        <Route path="/contact" component={ContactPage} />
+          <Route path="/projects/ADHD" component={ADHDProject} />
+          <Route path="/projects/heartbeats" component={HeartbeatProject} />
+          <Route path="/projects/trash" component={TrashFieldStudyProject} />
+        </div>
 
-        <Route path="/projects/ADHD" component={ADHDProject} />
-        <Route path="/projects/trash" component={TrashProject} />
-        <Route path="/projects/heartbeats" component={HeartbeatProject} />
-        <Route
-          path="/projects/trash-field-study"
-          component={TrashFieldStudyProject}
-        />
+        <Footer />
       </div>
-
-      <Footer />
     </div>
   </Router>
 );
