8e3fffe7a9869fdc47a63f4f7628b67a8db34d87
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2019-01-24 00:41:40 -0500

    some intensely productive deletions

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,41 +1,40 @@
 import React from 'react';
+import Divider from '../components/Divider';
 
 export default class Navigation extends React.Component {
   render = () => {
     return (
-      <div
-        class="navigation"
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          backgroundColor: 'white',
-        }}
-      >
-        <div>
-          <p
-          // style={{
-          //   display: 'flex',
-          //   flexDirection: 'column',
-          //   justifyContent: 'space-between',
-          //   alignItems: 'center',
-          // }}
-          >
-            <a href="/about">About</a> / <a href="/work">Work</a> /{' '}
-            <a href="/music">Music</a> / <a href="/contact">Contact</a>{' '}
-          </p>
+      <div>
+        <div
+          class="navigation"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+            padding: 10,
+            paddingBottom: 0,
+          }}
+        >
+          <div>
+            <p>
+              <a href="/about">About</a> / <a href="/#work">Work</a> /{' '}
+              <a href="/music">Music</a> / <a href="/contact">Contact</a>{' '}
+            </p>
+          </div>
+          <div>
+            <a
+              href="/"
+              style={{
+                color: 'grey',
+                margin: 0,
+              }}
+            >
+              rambhajan.me
+            </a>
+          </div>
         </div>
-        <div>
-          <a
-            href="/"
-            style={{
-              backgroundColor: 'white',
-              color: 'grey',
-            }}
-          >
-            rambhajan.me
-          </a>
-        </div>
+        <Divider />
       </div>
     );
   };
